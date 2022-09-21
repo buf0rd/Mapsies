926a5e08b15926fbc2c61545c185ae346c695d6f
buf0rd <buf0rd@buf0rd.com>
2022-09-21 11:30:08 -0500

    updating

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ I needed an easier way to associate malicious IP addresses with their geographic
 
 Requires:
 Installation of https://github.com/pieqq/PyGeoIpMap + DB/Map local to host. 
+
+Depends on:
+sudo apt install python3-numpy python3-matplotlib libgeos-dev python3-geoip2 python3-mpltoolkits.basemap 
+
--- mapsies.sh
+++ mapsies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ###the data for iframe
+
 ip="$(grep "Invalid" /var/log/auth.log* | sed -e 's/.*from\(.*\)port.*/\1/' | sort | uniq -c  | sort | tail -1  | awk '{print $2}')"
 attempts="$(grep "Invalid" /var/log/auth.log* | sed -e 's/.*from\(.*\)port.*/\1/' | sort | uniq -c  | sort | tail -1  | awk '{print $1}')"
 attackerip="$(grep "Invalid" /var/log/auth.log | tail -1 | grep -Eoa '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')"
